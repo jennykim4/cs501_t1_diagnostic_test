b7f19129c1f46facd89bf00d36401b37d18c698d
Jenny <jennykim@bu.edu>
2021-02-07 09:37:50 -0400

    first commit

---
+++ project/server/auth/views.py
@@ -0,0 +1,103 @@
+# project/server/auth/views.py
+
+from flask import Blueprint, request, make_response, jsonify
+from flask.views import MethodView, View
+
+from project.server import bcrypt, db
+from project.server.models import User
+
+from sqlalchemy import inspect
+
+auth_blueprint = Blueprint('auth', __name__)
+
+class RegisterAPI(MethodView):
+    """
+    User Registration Resource
+    """
+
+    def get(self):
+    	responseObject = {
+    		'status': 'success',
+    		'message': 'Request successful but please send an HTTP POST request to register the user.'
+    	}
+    	return make_response(jsonify(responseObject)), 201
+
+    def post(self):
+        # get the post data
+        post_data = request.get_json(force=True); print(request)
+        # check if user already exists
+        user = User.query.filter_by(email=post_data.get('email')).first()
+        if not user:
+            try:
+                user = User(
+                    email=post_data.get('email'),
+                    password=post_data.get('password')
+                )
+                # insert the user
+                db.session.add(user)
+                db.session.commit()
+                # generate the auth token
+                auth_token = user.encode_auth_token(user.id)
+                responseObject = {
+
+                    'status': 'success',
+                    'message': 'Successfully registered.',
+                    
+                    'auth_token': auth_token
+                }
+            
+                print (auth_token)
+                return make_response(jsonify(responseObject)), 201
+
+            except Exception as e:
+                print (e)
+                responseObject = {
+                    'status': 'fail',
+                    'message': 'Some error occurred. Please try again.'
+                }
+                return make_response(jsonify(responseObject)), 401
+        else:
+            responseObject = {
+                'status': 'fail',
+                'message': 'User already exists. Please Log in.',
+            }
+            return make_response(jsonify(responseObject)), 202
+
+
+# define the API resources
+registration_view = RegisterAPI.as_view('register_api')
+
+# add Rules for API Endpoints
+auth_blueprint.add_url_rule(
+    '/auth/register',
+    view_func=registration_view,
+    methods=['POST', 'GET']
+)
+
+
+
+class ShowUsersAPI(MethodView):
+    """
+    User List Resource
+    """
+
+    def get(self):
+        list_of_all_users = User.query.all()
+        users = [
+    		user.email for user in list_of_all_users
+    	]
+        print(users)
+        return make_response(jsonify(users)), 201
+       
+
+
+
+# define the API resources
+user_view = ShowUsersAPI.as_view('ShowUsers_api')
+
+# add Rules for API Endpoints
+auth_blueprint.add_url_rule(
+    '/users/index',
+    view_func=user_view,
+    methods=['GET']
+)
